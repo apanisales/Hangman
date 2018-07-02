6722eef44fec9f7a5795bf99e13ccffbb156b468
Anthony Panisales <31685417+apanisales@users.noreply.github.com>
2018-07-01 19:20:21 -0700

    Update hangman.py

--- hangman.py
+++ hangman.py
@@ -1,4 +1,20 @@
-# Author: Anthony Panisales
+#!/usr/bin/env python
+
+""" hangman.py
+
+Author: Anthony Panisales
+
+- Implementation of the game Hangman
+
+- The program reads most of the words found in the file hangmanWords.txt and after that, 
+the words can be used for the game. 
+
+- The game can be played by one person or multiple. 
+    - If it is played by multiple, one user can pick a word for the other players to solve, 
+    or a random word can be selected. 
+    - If the game is played by one person, a random word will automatically be selected for the player to solve.
+
+"""
 
 import random
 
@@ -52,7 +68,6 @@ def playHangman(word):
                         print("You lose! The word was '" + word + "'")
 
     print()
-
 
 def handleFile():
     hangmanList = []
